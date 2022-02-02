8188003618a42c1a5d17045f5f064c9c3ebf245e
mofe <mofeomotosho8@gmail.com>
2022-02-02 14:05:30 -0300

    first commit

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("", views.store, name="store"),
+    path("store/", views.store, name="store"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
+
+]
---
+++ store/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+
+
+def store(request):
+    context = {}
+    return render(
+        request, 'store/store.html', context
+    )
+
+
+def cart(request):
+    context = {}
+    return render(
+        request, 'store/cart.html', context
+    )
+
+def checkout(request):
+    context = {}
+    return render(
+        request, 'store/checkout.html', context
+    )
