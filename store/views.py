7e1fafe7ab0d23445265305959a64598c4a14db7
mofe <mofeomotosho8@gmail.com>
2022-02-07 16:39:11 -0300

    adding images to DB and rendering products on the site

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(
         request, 'store/store.html', context
     )
