d88bd6aec6adaee7114f2e34048c2a3093478fa9
mofe <mofeomotosho8@gmail.com>
2022-02-08 16:45:17 -0300

    rendering cart items from DB to cart

--- store/models.py
+++ store/models.py
@@ -49,12 +49,32 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def CartTtl(self):
+         orderitems = self.orderitem_set.all()
+         total = sum(item.ItemTttl for item in orderitems)
+         return total
+
+    @property
+    def CartItmTtl(self):
+        orderitems = self.orderitem_set.all()
+        total = sum(item.quantity for item in orderitems)
+        return total
+
+
+
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    @property
+    def ItemTttl(self):
+        total = self.product.price * self.quantity
+        return total
+
 
 
 class ShippingAddress(models.Model):
