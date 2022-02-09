3178b36b3d20a60d95f12e7ef6d46a3abc5beefb
mofe <mofeomotosho8@gmail.com>
2022-02-08 23:57:24 -0300

    rendering oder summary in checkout

--- store/views.py
+++ store/views.py
@@ -26,7 +26,16 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'CartTtl': 0, 'CartItmTtl': 0}
+    context = {'items': items, 'order': order}
+
+
     return render(
         request, 'store/checkout.html', context
     )
