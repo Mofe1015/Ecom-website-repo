757c52c5f1269b6c7960197089bf84bab832550a
mofe <mofeomotosho8@gmail.com>
2022-02-10 15:17:55 -0300

    adding event handlers and user verifaction to cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+var updateBts = document.getElementsByClassName('add-cart-btn')
+
+
+for(var i = 0; i < updateBts.length; i++){
+    updateBts[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action  = this.dataset.action
+        console.log('productId:', productId, 'action:',action)
+
+        console.log('USER:', user)
+        if (user === 'AnonymousUser'){
+            console.log('not logged in')
+        }else{
+            console.log('logged in and sending data ...')
+        }
+
+    })
+}
